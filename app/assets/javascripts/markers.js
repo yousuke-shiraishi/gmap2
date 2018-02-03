f6574aa7194c165a77cc9521fffc325946f40905
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2018-02-03 22:37:35 +0900

    マーカーの画像をポップアップに

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -20,54 +20,54 @@ function initMap() {
       },
       map: map,
       title: addresses[i].title,
-      icon: addresses[i].image,
       id: addresses[i].id
     });
+    marker.image = addresses[i].image;
     largeInfowindow[i] = new google.maps.InfoWindow({
       content: addresses[i].coment
     });
 
     m = marker;
-
-    markerEvent(i, largeInfowindow, m);
+    markerEvent(i, largeInfowindow, m, addresses[i].image);
 
     markers.push(m);
     bounds.extend(markers[i].position);
   }
   map.fitBounds(bounds);
 
+  var address;
 
-
-  function markerEvent(i, largeInfowindow, marker) {
+  function markerEvent(i, largeInfowindow, marker, address) {
     marker.addListener('click', function() { // マーカーをクリックしたとき
       if (largeInfowindow[i].marker != marker) {
         largeInfowindow[i].marker = marker;
-        largeInfowindow[i].setContent('<div><p>' + marker.title + '</p></div>' + '<div><p>' + largeInfowindow[i].content + '</p></div>');
+        largeInfowindow[i].setContent('<img class="picture" src=' + address["url"] + ' width="300px" height="300px" alt=""/>' + '<div><p>' + marker.title + '</p></div>' + '<div><p>' + largeInfowindow[i].content + '</p></div>');
         largeInfowindow[i].open(map, marker); // 吹き出しの表示
         largeInfowindow[i].addListener('closeclick', function() {
-           if(!!confirm('本当に削除しますか?削除すると戻せません。')){
-          $.ajax({
-            url: 'gmaps/destroy_images',
-            type: 'POST',
-            data: //{
-              ('marker=' + `${marker.id}`),
-            //_method: 'DELETE'
-            processData: false, //おまじない
-            contentType: false, //おまじない
-            dataType: 'json' //データ形式を指定
-            //  },datatype: "html"
-          }).then(
-            function(data) {
-              alert('削除に成功しました。');
-            },
-            function() {
-              alert('他の人のマーカー情報は消せません。');
-            });
+          if (!!confirm('本当に削除しますか?削除すると戻せません。')) {
+            $.ajax({
+              url: 'gmaps/destroy_images',
+              type: 'POST',
+              data: //{
+                ('marker=' + `${marker.id}`),
+              //_method: 'DELETE'
+              processData: false, //おまじない
+              contentType: false, //おまじない
+              dataType: 'json' //データ形式を指定
+              //  },datatype: "html"
+            }).then(
+              function(data) {
+                alert('削除に成功しました。');
+              },
+              function() {
+                alert('他の人のマーカー情報は消せません。');
+              });
 
           }
-          marker.setMap(null);
-          marker = null;
-
+          if (largeInfowindow[i].marker == marker) {
+            marker.setMap(null);
+            marker = null;
+          }
         });
       }
     });
