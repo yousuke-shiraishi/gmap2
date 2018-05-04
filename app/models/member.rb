4b83831ecfed0353f81491747e669335f33dc23f
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2018-05-04 17:43:29 +0900

    cloudinaryと連携

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,26 +1,27 @@
 class Member < ApplicationRecord
-  require 'date'
+  attr_accessor :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
- validates :name,  presence: true, length: { maximum: 30 }
- validates :birth, presence: true
- # validate :date_valid?
- has_many :gmaps
- mount_uploader :image, ImageUploader
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :birth, presence: true
+  # validate :date_valid?
+  has_many :gmaps
+  mount_uploader :image, ImageUploader
 
- private
- # def date_valid?
- #   errors.add(:birth,'正しい生年月日ではありません')
- #     unless
- #         birth_tmp = birth
- #         birth = birth_tmp if birth.tr!("０-９","0-9") == nil
- #     if /(\d{4})年(\d{1,2})月(\d{1,2})日/ =~ birth
- #         birth =[$1,$2,$3].join("/")
- #     end
- #       birth = birth.to_s
- #       !! Date.parse(birth) rescue false
- #     end
- # end
+  private
+
+  # def date_valid?
+  #   errors.add(:birth,'正しい生年月日ではありません')
+  #     unless
+  #         birth_tmp = birth
+  #         birth = birth_tmp if birth.tr!("０-９","0-9") == nil
+  #     if /(\d{4})年(\d{1,2})月(\d{1,2})日/ =~ birth
+  #         birth =[$1,$2,$3].join("/")
+  #     end
+  #       birth = birth.to_s
+  #       !! Date.parse(birth) rescue false
+  #     end
+  # end
 end
